b9c61007022010d66bdda35bea5c4c07d38580a1
Heather Marie Haas <osxgirl@mac.com>
2017-11-14 00:56:58 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,16 +7,21 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index(user_input)
-    user_input.to_i - 1
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def move( board, input_to_index, players_character = "X" )
-    board[input_to_index] = players_character
-  return board
+# code your input_to_index and move method here!
+
+def input_to_index(input)
+  index = input.to_i - 1
 end
 
-def move(board, index, "X")
+def move(board, index, token="X")
+  board[index] = token
+  board
 end
-
-display_board(board, move, index)
